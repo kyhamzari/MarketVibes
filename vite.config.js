0fcf97856a80cb92a6aae778d5a15ab7829f5f30
kyhamzari <kyhamzari@gmail.com>
2026-06-13 11:47:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,3 +18,10 @@ export default defineConfig({
     react(),
   ]
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/kyhamzari/MarketVibes/', // 👈 Replace with your exact repository name
+})
